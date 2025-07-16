d5d4c28c391377e62848cb3e4331af3a6ca2f812
NITHIN <nithingedda45@gmail.com>
2025-07-16 23:12:22 +0530

    EDA & Model Performance

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys 
-import logging
-from logging import *
+from src.logger import logging
 
 def error_msg_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -14,7 +13,7 @@ class CustomException(Exception):
     def __init__(self,error_msg,error_detail:sys):
         super().__init__(error_msg)
         self.error_msg = error_msg_detail(error_msg,error_detail=error_detail)
-        logging.error(self.error_msg)
+        
 
     def __str__(self):
         return self.error_msg
